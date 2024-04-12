1ab56470af8f8898f74cc89039a43c54f2ef670e
aatbip <anantabipal2010@gmail.com>
2024-04-13 02:02:53 +0545

    add: homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,6 @@ import { Typography } from "@mui/material";
 
 export default function Home() {
   return (
-    <Typography variant="Caption Text">Hey!</Typography>
+    <Typography variant="heading1">Hey!</Typography>
   );
 }
