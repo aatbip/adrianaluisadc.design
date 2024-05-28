9051cdb064a43d0a5339e24004334a1e0609c978
Adriana Luisa Dela Cruz <adrianaluisadelacruz@gmail.com>
2024-05-29 00:53:00 +0545

    fix build issue

--- src/app/about-me/page.tsx
+++ src/app/about-me/page.tsx
@@ -1,33 +1,34 @@
-import { Difference } from "@mui/icons-material";
-import { Typography,Box } from "@mui/material";
+// import { Difference } from "@mui/icons-material";
+// import { Typography,Box } from "@mui/material";
 
+// export default async function AboutPage() {
+//     return(
+//  <Box sx={{
+//     backgroundColor: 'color.background.primary',
+//     height: '100vh',
 
-export default async function AboutPage() {
-    return(
- <Box sx={{
-    backgroundColor: 'color.background.primary',
-    height: '100vh',
+//  }}>
+// <Typography variant="heading1" sx={{
 
- }}>      
-<Typography variant="heading1" sx={{
-   
-    color: '#F4F4F4',
-    mixBlendMode: "Difference",
-}}>
+//     color: '#F4F4F4',
+//     mixBlendMode: "Difference",
+// }}>
 
-    About Me
-</Typography>
+//     About Me
+// </Typography>
 
+// <Typography variant="heading2" sx={{
+//     color: '#F4F4F4',
+//     mixBlendMode: "Difference",
+// }}>
 
-<Typography variant="heading2" sx={{
-    color: '#F4F4F4',
-    mixBlendMode: "Difference",
-}}>
+//    Hi everyone! I am Adriana
+// </Typography>
+// </Box>
 
-   Hi everyone! I'm Adriana
-</Typography>
-</Box> 
+//     )
+//   }
 
-    )
-  }
-  
+export default async function AboutPage () {
+    return <h1>not yet ready</h1>
+}
