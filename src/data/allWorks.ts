fb6e873c85905c1cfc08c601af600041e1745ef4
Adriana Luisa Dela Cruz <adrianaluisadelacruz@gmail.com>
2024-05-30 17:43:50 +0545

    [fix] category

--- src/data/allWorks.ts
+++ src/data/allWorks.ts
@@ -2,27 +2,27 @@ import { IBasicCardProps } from "@/types/interfaces"
 
 export const allWorks: IBasicCardProps[] = [
   {
-    caption: '2023 / UI DESIGN',
+    caption: '2023 / USER INTERFACE',
     title: 'Boxy Fighters Platform | Profile Page',
     imagePath: 'Boxy/banner.png',
     pathName: 'Boxy'
   },
 
   {
-    caption: '2023 / Branding',
+    caption: '2023 / BRAND',
     title: 'Charity Ball',
     imagePath: 'CharityBall/banner.png',
     pathName: 'CharityBall'
   },
   {
-    caption: '2023 / UI DESIGN',
+    caption: '2023 / USER INTERFACE',
     title: 'Dispense Go',
     imagePath: 'DispenseGo/main.png',
     pathName: 'DispenseGo'
   },
 
   {
-    caption: '2023 / UI Design',
+    caption: '2023 / USER INTERFACE',
     title: 'J80 Fitness',
     imagePath: 'J80Fitness/cover.png',
     pathName: 'J80Fitness'
@@ -34,20 +34,20 @@ export const allWorks: IBasicCardProps[] = [
   //   pathName: 'Gliddr'
   // },
   {
-    caption: '2023 / UI DESIGN',
+    caption: '2023 / USER INTERFACE',
     title: 'GSweet Chrome AI Extension',
     imagePath: 'GSweet/banner.png',
     pathName: 'GSweet'
   },
   {
-    caption: '2023 / Branding',
+    caption: '2023 / BRAND',
     title: 'Spendidly',
     imagePath: 'Spendidly/banner.png',
     pathName: 'Spendidly' 
   },
 
   {
-    caption: '2022 / BRAND DESIGN',
+    caption: '2022 / BRAND',
     title: 'Nepal Air Trip Brand Guidelines',
     imagePath: 'NepalAirTripBrand/Cover.png',
     pathName: 'NepalAirTripBrand'
