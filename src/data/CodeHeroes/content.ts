db44c2bda25c630f1199cea6ff9c75de309ad7da
Adriana Luisa Dela Cruz <adrianaluisadelacruz@gmail.com>
2024-05-30 10:06:17 +0545

    [add] CodeHeroes Project

---
+++ src/data/CodeHeroes/content.ts
@@ -0,0 +1,46 @@
+import { IContent } from "@/types/contentRendererTypes";
+import { blockHeaderCreator, blockRendererCreator, bulletListCreator, headingWithUnderlineCreator, imageHolderCreator, mainTitleCreator, paragraphCreator, primaryTitleCreator, secondaryTitleCreator } from "@/utils/contentRendererObjectCreators";
+
+export const content: IContent = [
+  mainTitleCreator({
+    title: "Codeheroes",
+    ctaButton1Content: "View Prototype",
+    ctaButton1Link: "https://www.figma.com/proto/oChmQIOqvrint2CNHskggC/Portfolio-%26-Resume-2024?page-id=1%3A15&node-id=234-76419&viewport=-4053%2C1266%2C0.19&t=FQgKlLKurrwyVSdq-1&scaling=min-zoom",
+    // ctaButton2Content: "SEE THIS PROJECT LIVE",
+    // ctaButton2Link: "https://boxy.com"
+  }),
+
+  secondaryTitleCreator({
+    content: "DESIGN SHOWCASE"
+  }),
+
+  imageHolderCreator({
+    imagePath: "CodeHeroes/1.png",
+    noMargin: false
+  }),
+  imageHolderCreator({
+    imagePath: "CodeHeroes/2.png",
+    noMargin: false
+  }),
+  imageHolderCreator({
+    imagePath: "CodeHeroes/3.png",
+    noMargin: false
+  }),
+  imageHolderCreator({
+    imagePath: "CodeHeroes/4.png",
+    noMargin: false
+  }),
+  imageHolderCreator({
+    imagePath: "CodeHeroes/5.png",
+    noMargin: false
+  }),
+  imageHolderCreator({
+    imagePath: "CodeHeroes/6.png",
+    noMargin: false
+  }),
+  imageHolderCreator({
+    imagePath: "CodeHeroes/7.png",
+    noMargin: false
+  }),
+]
+
--- src/data/uiExplorations.ts
+++ src/data/uiExplorations.ts
@@ -2,9 +2,17 @@ import { IBasicCardProps } from "@/types/interfaces"
 
 export const uiExplorations: IBasicCardProps[] = [
   {
+    caption: '2024 / UI DESIGN',
+    title: 'CodeHeroes',
+    imagePath: 'CodeHeroes/cover.png',
+    pathName: 'CodeHeroes'
+  },
+  
+  {
     caption: '2022 / UI DESIGN',
     title: 'Maminao',
     imagePath: 'Maminao/cover.png',
     pathName: 'Maminao'
   }
+ 
 ]
